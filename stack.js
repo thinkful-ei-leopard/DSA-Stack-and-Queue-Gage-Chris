f4474629193ca1536142a5670dd0f803e62e43b7
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-03 13:21:16 -0500

    Finished stack exercises

--- stack.js
+++ stack.js
@@ -16,10 +16,127 @@ class Stack {
     }
     const node = new _Node(item, this.top);
     this.top = node;
+    return this.top;
   }
   pop() {
-    const node = this.this.top;
+    const node = this.top;
     this.top = node.next;
     return node.data;
   }
 }
+
+function peek(stack) {
+  return stack.top;
+}
+
+function isEmpty(stack) {
+  if(stack.top === null) {
+    return true;
+  }
+  return false;
+}
+
+function display(stack) {
+  let currNode = stack.top;
+  let list = '';
+  while (currNode.next !== null) {
+    list += `${currNode.value} => `;
+    currNode = currNode.next;
+  }
+  list += `${currNode.value}`;
+  return list;
+}
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let stack = new Stack;
+
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s.charAt(i));
+  }
+
+  let stackString = '';
+  let currNode = stack.top;
+  while (currNode.next !== null) {
+    stackString += currNode.value;
+    currNode = currNode.next;
+  }
+  stackString += currNode.value;
+
+  if (s === stackString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function matchingParenths(string) {
+  let stack = new Stack;
+  let error = null;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") {
+      if (stack.top === null) {
+        stack.push(string[i]);
+      } else if (stack.top === ")") {
+        stack.push(string[i]);
+      } else if (stack.top !==null && stack.top !== ")") {
+        error = `String missing a ')' before '(' at index ${i}`;
+      }
+    }
+    if (string[i] === ")" && stack.top !== "(") {
+      error = `String missing a '(' before ')' at index ${i}`;
+    } else  if (string[i] === ")"){
+      stack.push(string[i]);
+    }
+  }
+  if (error) {
+    return error;
+  } else {
+    return 'String checks out!';
+  }
+}
+
+function matchingParenths2(string){
+  let stack = new Stack;
+  let error = null;
+
+  for (let i= 0; i < string.length;i ++){
+    if(string[i] === '('){
+      stack.push(i);
+    } else if ((string[i] === ')')) {
+      if (stack.top !== null){
+        stack.pop();
+      } else {
+        error = `You have an unexpected closing parentheses at index ${i}`;
+      }
+    } 
+  }
+
+  if(stack.top !== null){
+    error = `You have an unexpected open parentheses at index ${stack.top.value}`;
+  }
+
+  if (error === null){
+    return 'String checks out!';
+  } else {
+    return error;
+  }
+}
+
+function main() {
+  let starTrek = new Stack;
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  starTrek.pop();
+  starTrek.pop();
+  console.log(matchingParenths2('())'));
+  console.log(matchingParenths2('(('));
+  console.log(matchingParenths2('()'));
+  console.log(matchingParenths2('(Parentheses) are cool'));
+  return display(starTrek);
+}
+
+console.log(main());

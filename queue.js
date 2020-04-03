5f2735201e4e8554d84ddc75210986e3805971fb
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-03 10:40:04 -0500

    Added Stack and Queue classes

--- queue.js
+++ queue.js
@@ -10,4 +10,25 @@ class Queue {
     this.first = null;
     this.last = null;
   }
+  enqueue(item) {
+    const node = new _Node(item);
+    if (this.first === null) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
+  }
+  dequeue() {
+    if(this.first === null){
+      return;
+    }
+    const node =this.first;
+    this.first = this.first.next;
+    if (node === this.last){
+      this.last = null;
+    }
+    return node.value;
+  }
 }
--- stack.js
+++ stack.js
@@ -9,4 +9,17 @@ class Stack {
   constructor() {
     this.top = null;
   }
+  push(item) {
+    if (this.top === null) {
+      this.top = new _Node(item);
+      return this.top;
+    }
+    const node = new _Node(item, this.top);
+    this.top = node;
+  }
+  pop() {
+    const node = this.this.top;
+    this.top = node.next;
+    return node.data;
+  }
 }
